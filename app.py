5fa61f2af57e75a2768a3e93afe325e47a89fe91
Otávio <netootavio223@outlook.com>
2024-09-17 15:36:26 -0300

    estruturação de pastas

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from model.model import model_blueprint
 from home.home import home_blueprint
+from database.conection import database_blueprint
 
 app = Flask(__name__)
 app.secret_key = 'equipTrack'
 
 app.register_blueprint(model_blueprint)
 app.register_blueprint(home_blueprint)
+app.register_blueprint(database_blueprint)
 
 
 if __name__ == '__main__':
---
+++ database/conection.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+database_blueprint = Blueprint('database', __name__)
