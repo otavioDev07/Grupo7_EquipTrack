0dbc7a1b7838f0fcbf443d6f8fd4aa0ba52e9f1b
PedroSchim1dt <pedro.schimidtt@gmail.com>
2024-10-04 13:32:37 -0300

    tela de login (somente front)

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from model.model import model_blueprint
 from home.home import home_blueprint
 from database.conection import database_blueprint
+from login.login import login_blueprint
 
 app = Flask(__name__)
 app.secret_key = 'equipTrack'
@@ -9,6 +10,7 @@ app.secret_key = 'equipTrack'
 app.register_blueprint(model_blueprint)
 app.register_blueprint(home_blueprint)
 app.register_blueprint(database_blueprint)
+app.register_blueprint(login_blueprint)
 
 
 if __name__ == '__main__':
