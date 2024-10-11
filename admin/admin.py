77f26386b8fd6101d4e20821da7451c32b313673
samanthasenai <samantha.almeida.senai@gmail.com>
2024-10-11 09:08:56 -0300

    .

--- admin/admin.py
+++ admin/admin.py
@@ -4,4 +4,4 @@ admin_blueprint = Blueprint('admin', __name__, template_folder="templates")
 
 @admin_blueprint.route('/cadastroEPI')
 def cadastro_EPI():
-    return render_template('#')
+    return render_template('cadastroEPI.html')
