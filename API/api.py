8cfb8885de369fd7ff1d94fa0a8a6d459fd68727
Otávio <netootavio223@outlook.com>
2024-10-11 14:12:03 -0300

    listEPI e listFuncionários (API)

--- API/api.py
+++ API/api.py
@@ -10,26 +10,17 @@ CORS(app)
 def list_EPI():
     with conecta_db() as (conexao, cursor):
         try:
-            cursor.execute('SELECT  FROM epi')
+            cursor.execute('SELECT idEPI, codigoCA, nomeEquipamento, quantidade, status FROM epi')
             result = cursor.fetchall()
             
             estoque = []
             for row in result:
                 estoque.append({
                     'idEPI': row[0],
-                    'numSerieEPI': row[1],
-                    'marca': row[2],
-                    'modelo': row[3],
-                    'dataLocacao': row[4],
-                    'dataVencimento': row[5],
-                    'status': row[6],
-                    'observacoes': row[7],
-                    'nomeEquipamento': row[8],
-                    'dataAquisicao': row[9],
-                    'tamanho': row[10],
-                    'idSetor': row[11],
-                    'idCategoria': row[12],
-                    'quantidade': row[13]
+                    'codigoCA': row[1],
+                    'nomeEquipamento': row[2],
+                    'quantidade': row [3],
+                    'status': row[4]
                 })
 
             return jsonify(estoque), 200 
@@ -38,25 +29,20 @@ def list_EPI():
             return jsonify({'error': str(e)}), 400
         
 #Listagem dos funcionários
-@app.route('/listFuncionarios', methods=['GET'])
-def list_funcionarios():
+@app.route('/listFuncionarios/<id>', methods=['GET'])
+def list_funcionarios(id):
     with conecta_db() as (conexao, cursor):
         try:
-            cursor.execute('SELECT * FROM funcionário')
+            cursor.execute(f'SELECT idFuncionario, nomeFuncionário, NIF, cargo FROM funcionário WHERE idSetor = {id}')
             result = cursor.fetchall()
 
             funcionarios = []
             for row in result:
                 funcionarios.append({
                     'idFuncionario': row[0],
-                    'nomeFuncionario': row[1],
+                    'nomeFuncionário': row[1],
                     'NIF': row[2],
-                    'CPF': row[3],
-                    'idSetor': row[4],
-                    'condicoes_especiais': row[5],
-                    'cargo': row[6],
-                    'tamCalcado': row[7],
-                    'tamRoupa': row[8]
+                    'cargo': row[3],
                 })
             return jsonify(funcionarios), 200 
         except Exception as e:
