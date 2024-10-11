5f907e8007e4a24e69aefc0eefbeb8afcfdb46f1
PedroSchim1dt <pedro.schimidtt@gmail.com>
2024-10-11 13:16:11 -0300

    pagina alocar atualizada

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from model.model import model_blueprint
 from home.home import home_blueprint
 from database.conection import db_blueprint
 from login.login import login_blueprint
+from alocar.alocar import alocar_blueprint
+
 
 app = Flask(__name__)
 app.secret_key = 'equipTrack'
@@ -11,7 +13,7 @@ app.register_blueprint(model_blueprint)
 app.register_blueprint(home_blueprint)
 app.register_blueprint(db_blueprint)
 app.register_blueprint(login_blueprint)
-
+app.register_blueprint(alocar_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True)
