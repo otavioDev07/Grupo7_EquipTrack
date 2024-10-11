f94b12ab93abbf94991c8da4b92b7bae3254571d
PedroSchim1dt <pedro.schimidtt@gmail.com>
2024-10-11 09:55:31 -0300

    ...

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from model.model import model_blueprint
 from home.home import home_blueprint
-from database.conection import database_blueprint
+from database.conection import db_blueprint
 from login.login import login_blueprint
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ app.secret_key = 'equipTrack'
 
 app.register_blueprint(model_blueprint)
 app.register_blueprint(home_blueprint)
-app.register_blueprint(database_blueprint)
+app.register_blueprint(db_blueprint)
 app.register_blueprint(login_blueprint)
 
 
