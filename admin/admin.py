7d94e87ef6c3e61f2350f480a9095015b6a0a69c
Sara <sara.resende.senai@gmail.com>
2024-10-11 12:25:01 -0300

    cadastro de func

--- admin/admin.py
+++ admin/admin.py
@@ -5,3 +5,7 @@ admin_blueprint = Blueprint('admin', __name__, template_folder="templates")
 @admin_blueprint.route('/cadastroEPI')
 def cadastro_EPI():
     return render_template('cadastroEPI.html')
+
+@admin_blueprint.route('/cadastroFuncionario')
+def cadastro_Funcionario():
+    return render_template('cadastroFuncionario.html')
