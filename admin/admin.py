3f11f6860e6123911a4b3fa0c2943c7f816af6fa
Otávio <netootavio223@outlook.com>
2024-10-11 08:29:08 -0300

    criação da pasta adm

---
+++ admin/admin.py
@@ -0,0 +1,7 @@
+from flask import render_template, Blueprint
+
+admin_blueprint = Blueprint('admin', __name__, template_folder="templates")
+
+@admin_blueprint.route('/cadastroEPI')
+def cadastro_EPI():
+    return render_template('#')
