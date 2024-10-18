7834c817eff5356aac1a81af6d4ae0a0ab7cc076
Sara <sara.resende.senai@gmail.com>
2024-10-18 09:26:02 -0300

    cadastroDescarte

--- admin/admin.py
+++ admin/admin.py
@@ -13,3 +13,7 @@ def cadastro_Funcionario():
 @admin_blueprint.route('/descarte')
 def descarte():
     return render_template('descarte.html')
+
+@admin_blueprint.route('/cadastroDescarte')
+def cadastroDescarte():
+    return render_template('cadastroDescarte.html')
