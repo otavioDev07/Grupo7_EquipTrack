351edcf14243c1b4faea1cc4fdfd530d0a8534fe
Otávio <netootavio223@outlook.com>
2024-11-05 15:24:04 -0300

    correção

--- estoqueEPI/estoqueEPI.py
+++ estoqueEPI/estoqueEPI.py
@@ -5,20 +5,22 @@ estoque_blueprint = Blueprint('estoqueEPI', __name__, template_folder="templates
 
 @estoque_blueprint.route('/estoque', methods=['GET'])
 def get_estoque():
-    filtro = request.args.get('filtro', 'no_prazo')  #'no_prazo' como padrão
+    filtro = request.args.get('filtro', 'no_prazo')  # 'no_prazo' como padrão
     query = 'SELECT idEPI, codigoCA, nomeEquipamento, quantidade FROM epi'
     
     if filtro == 'no_prazo':
         query += ' WHERE dataVencimento > CURDATE() + INTERVAL 30 DAY' 
     elif filtro == 'perto_vencimento':
-        query += ' WHERE dataVencimento BETWEEN CURDATE() AND CURDATE() + INTERVAL 30 DAY'  # Exemplo para "Perto do vencimento"
+        query += ' WHERE dataVencimento BETWEEN CURDATE() AND CURDATE() + INTERVAL 30 DAY'
     elif filtro == 'vencido':
-        query += ' WHERE dataVencimento < CURDATE()'  
+        query += ' WHERE dataVencimento < CURDATE()'
+    
     try:
         with conecta_db() as (conexao, cursor):
             cursor.execute(query)
             EPIs = cursor.fetchall()
-            return render_template('estoqueEPI.html', EPIs=EPIs)
+            return render_template('estoqueEPI.html', EPIs=EPIs, filtro=filtro)
     except Exception as e:
-        return print("Erro ao buscar dados:", e)
+        print("Erro ao buscar dados:", e)
+        return "Erro ao buscar dados", 500
         
