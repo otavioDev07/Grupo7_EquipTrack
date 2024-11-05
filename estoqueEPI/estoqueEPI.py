320a5166199f07d76024b0144ad5a6410bc92d8b
Otávio <netootavio223@outlook.com>
2024-11-05 13:10:31 -0300

    página carregando agora

--- estoqueEPI/estoqueEPI.py
+++ estoqueEPI/estoqueEPI.py
@@ -5,22 +5,23 @@ estoque_blueprint = Blueprint('estoqueEPI', __name__, template_folder="templates
 
 @estoque_blueprint.route('/estoque', methods=['GET'])
 def get_estoque():
-    def get_estoque():
-        filtro = request.args.get('filtro', 'no_prazo')  #'no_prazo' como padrão
-        query = 'SELECT idEPI, codigoCA, nomeEquipamento, quantidade FROM epi'
-        
-        if filtro == 'no_prazo':
-            query += ' WHERE data_vencimento > CURDATE() + INTERVAL 30 DAY' 
-        elif filtro == 'perto_vencimento':
-            query += ' WHERE data_vencimento BETWEEN CURDATE() AND CURDATE() + INTERVAL 30 DAY'  # Exemplo para "Perto do vencimento"
-        elif filtro == 'vencido':
-            query += ' WHERE data_vencimento < CURDATE()'  
+    filtro = request.args.get('filtro', 'no_prazo')  #'no_prazo' como padrão
+    query = 'SELECT idEPI, codigoCA, nomeEquipamento, quantidade FROM epi'
+    
+    if filtro == 'no_prazo':
+        query += ' WHERE dataVencimento > CURDATE() + INTERVAL 30 DAY' 
+    elif filtro == 'perto_vencimento':
+        query += ' WHERE dataVencimento BETWEEN CURDATE() AND CURDATE() + INTERVAL 30 DAY'  # Exemplo para "Perto do vencimento"
+    elif filtro == 'vencido':
+        query += ' WHERE dataVencimento < CURDATE()'  
+    print('chegou')
 
-        try:
-            with conecta_db() as (conexao, cursor):
-                cursor.execute(query)
-                EPIs = cursor.fetchall()
-                return render_template('estoque.html', EPIs=EPIs)
-        except Exception as e:
-            print("Erro ao buscar dados:", e)
-            return print('erro')
+    try:
+        with conecta_db() as (conexao, cursor):
+            cursor.execute(query)
+            EPIs = cursor.fetchall()
+            return render_template('estoqueEPI.html', EPIs=EPIs)
+        print('chegou 2')
+    except Exception as e:
+        return print("Erro ao buscar dados:", e)
+        
